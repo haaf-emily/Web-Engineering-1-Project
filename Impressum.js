a283264371b6c7c7f2fcbeedb78645512fb2aa42
carolina-cza <car.czapla.23@lehre.mosbach.dhbw.de>
2024-06-18 14:14:05 +0200

    Elinas Code

---
+++ Impressum.js
@@ -0,0 +1,8 @@
+function toggleImprint() {
+    var content = document.getElementById("impressumContent");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
