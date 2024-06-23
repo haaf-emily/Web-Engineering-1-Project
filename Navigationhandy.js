cb7b38847fdf8a63025413925487ab05885e86c2
haaf-emily <emily.haaf05@gmail.com>
2024-06-23 19:51:55 +0200

    Delet

--- Navigationhandy.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var submenu = document.querySelector('.submenu');
-    var submenuContent = document.querySelector('.submenu-content');
-
-    submenu.addEventListener('click', function (event) {
-        event.preventDefault();
-        submenuContent.classList.toggle('show');
-    });
-});
